63c4963b9572069fdd99be46eaf6e5022f3e44f0
Sergiu Muntean <eu.msergiu@gmail.com>
2022-07-15 13:52:51 +0300

    [US#2] change mapping name

--- src/main/java/com/garagesale/controller/AssetController.java
+++ src/main/java/com/garagesale/controller/AssetController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Asset")
+@RequestMapping("/asset")
 public class AssetController {
 
     private AssetService assetService;
