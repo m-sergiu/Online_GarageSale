31c8a0a64970c720d79ddf863e17616e3d14fafa
Sergiu Muntean <eu.msergiu@gmail.com>
2022-08-09 17:29:20 +0300

    [US#9] - feedback

--- src/main/java/com/garagesale/service/OrderServiceImpl.java
+++ src/main/java/com/garagesale/service/OrderServiceImpl.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 @Service
